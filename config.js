e5cc73abe6ae2fa254b632cd18e112727c81a51c
jay8409 <jaysunghere@gmail.com>
2026-08-15 11:22:37 +0900

    Deploy config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+/**
+ * Vercel & Supabase Cloud Connection Configuration for Todo MVP App
+ */
+
+window.SUPABASE_CONFIG = {
+  url: "https://rsaeufjlgpuwsttvplii.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJzYWV1ZmpsZ3B1d3N0dHZwbGlpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYxMzYwNDMsImV4cCI6MjEwMTcxMjA0M30.rRk3IGIcDLRPPSvp_wHHyRoDjIkkrAnd3UBEZHyYUJ4"
+};
